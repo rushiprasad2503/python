692741c415222687441ccb801d3c0107f15c743b
Rushiprasad Pawale <rushiprasadpawale25@email.com>
2025-08-10 16:14:29 +0530

    yea

---
+++ data type/set/built in .py
@@ -0,0 +1,13 @@
+s = {'apple', 'banan dsxaf^so', 'banan dsxaf@'}
+for index, value in enumerate(s):
+    print(index, value)
+print(enumerate(s))
+print(len(s))
+print(max(s))
+print(min(s))
+print(sorted(s))
+#print(sum(s)) for only numbers
+s.add('jje')
+print(s)
+hj = sorted(s)
+print(hj)
---
+++ data type/set/remove.py
@@ -0,0 +1,6 @@
+std = {1,2,3,4,555,6,6}
+jds = std.discard(555)
+print(std)
+d = {'1','2','3'}
+g = d.discard('3')
+print(d)# discarding element from set
